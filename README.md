8036001a2ac2abbc31c94054fa0077b496420193
JPStrydom <jpstrydom8@gmail.com>
2019-05-03 14:06:33 +0200

    Add wildcard checker tool

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ This is a simple tool that ensures your Node.js dependencies are wildcard (^) fr
 
 ## Description
 
-A wildcard in a dependancy version indicates that that dependence has to be the specified version or higher - which means that two `npm install`/`yarn install` commands could result in two diffirent versions of the dependancy being installed. This could introduce unwanted errors, so it is good practice to ensure your dependencies are wildcard free. 
+A wildcard in a dependency version indicates that that dependence has to be the specified version or higher - which means that two `npm install`/`yarn install` commands could result in two different versions of the dependency being installed. This could introduce unwanted errors, so it is good practice to ensure your dependencies are wildcard free. 
 
-This tool checks all your dependancies (including devDependancies) and will fail if any of them contain a wildcard. It will also notify as to where the wildcards are, which makes it easy to detect and correct them. 
+This tool checks all your dependencies (including devDependencies) and will fail if any of them contain a wildcard. It will also notify as to where the wildcards are, which makes it easy to detect and correct them. 
 
 This tool is thus ideal for pre-commit hooks and linting steps.
 
---
+++ index.js
@@ -0,0 +1,45 @@
+const chalk = require('chalk');
+
+const packageJsonDir = process.argv.slice(2)[0];
+
+const packageJson = require(packageJsonDir || '../../package.json');
+
+log('Checking dependencies for wildcards');
+
+const dependencySections = [packageJson.dependencies, packageJson.devDependencies];
+dependencySections.forEach(checkDependencySection);
+
+log('All dependencies are wildcard free', 'success');
+
+function checkDependencySection(dependencySection) {
+    Object.keys(dependencySection).forEach(dependencyName => {
+        const versionString = dependencySection[dependencyName];
+        if (versionString.startsWith('http')) {
+            log(`HTTP dependency detected (${versionString}), skipping.`, 'warning');
+            return;
+        }
+        const allowedCharacters = /^[a-zA-Z\d.-]+$/;
+        const valid = allowedCharacters.test(versionString);
+        if (!valid) {
+            const errorMessage = `Dependency ${dependencyName} has a version string (${versionString}) with invalid characters`;
+            log(errorMessage, 'error');
+            throw errorMessage;
+        }
+    });
+}
+
+function log(message, type) {
+    switch (type) {
+        case 'error':
+            console.log(chalk.red(message));
+            break;
+        case 'warning':
+            console.log(chalk.yellow(message));
+            break;
+        case 'success':
+            console.log(chalk.green(message));
+            break;
+        default:
+            console.log(chalk.cyan(message));
+    }
+}
